7997582adab4f2939fb8e5220b0b271e5eadf830
Masralai <devdeeppaul2004@gmail.com>
2026-01-17 03:59:17 +0530

    action execution

--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 )
 
 func main(){
-	(&cli.Command{}).Run(context.Background(), os.Args)
+	cmd:= &cli.Command{
+		Name: "boom",
+		Usage: "make an explosive entrance",
+		Action: func(context.Context, *cli.Command)error{
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+	
+	if err:= cmd.Run(context.Background(),os.Args);err!=nil{
+		log.Fatal(err)
+	}
 }
 
